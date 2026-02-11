9017d949a0abacf9eccc16e645421af4cddf0f77
nashabil20241 <libanahmad08@gmail.com>
2026-02-11 11:01:21 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,5 @@
 <?php
-$merchant_id = "M260210CSXG7601QA"; 
-$secret_key  = "820fffda80a16d4ef1ac9ff219bee76f4fb04e31fefa2f65b943d1d3bae8801c"; 
+// File: koneksi.php
+$api_id  = "NViFmNJa"; 
+$api_key = "xGIcBhEZjjKhTIR2AhugoE0GnFX0yJJH3saa4M7c7QQyvfh4HbmjpCsYd26unOJM"; 
 ?>
