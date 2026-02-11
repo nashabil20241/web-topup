b4b80f5c9227b606dbca2a10b1c9556fb8e77bcc
nashabil20241 <libanahmad08@gmail.com>
2026-02-11 10:09:03 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,9 +2,4 @@
 // File: koneksi.php
 $merchant_id = "M260210CSXG7601QA"; 
 $secret_key  = "fe82a9adfea474e2ba25bd42f676cc5f478d84b2dd4bf9401e84dba24e4c6ef9"; 
-
-// Koneksi ke Database Railway (Opsional jika Mas pakai DB)
-// $host = $_ENV['MYSQLHOST']; 
-// $user = $_ENV['MYSQLUSER'];
-// ... dst
 ?>
